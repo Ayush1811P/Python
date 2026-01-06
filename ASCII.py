d4833e3cbe997de96252dbd92fe1bb2059e9bfd6
Ayush Singh <awsmsi0405@gmail.com>
2026-01-07 00:01:48 +0530

    ASCII

---
+++ ASCII.py
@@ -0,0 +1,40 @@
+       #WORKING WITH ASCII
+
+
+# ch = input("Enter a character: ")
+# if len(ch)!=1:
+#     print("enter only one character!")
+# else:
+#     ascii=ord(ch)    #ord stands for ordinal value
+#     if ascii >=65 and ascii <=90:
+#         print("Uppercase Alfabet ",ch)
+#         print("The ASCII value is: ",ascii)
+#     elif ascii >97 and ascii < 122:
+#         print("lowercase Alfabet ",ch)  
+#         print("The ASCII value is: ",ascii) 
+#     else:
+#         print("Special character: ",ch)
+#         print("The ASCII value is: ",ascii)  
+#   
+
+print(ord("H"))
+print(ord("h"))
+
+ch = input("Enter the character: ")
+A=65
+Z=90
+CH= ch.upper()
+value=65
+Alfabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+if len(ch)!=1 and len(CH)!=1:
+        print("ENter one alfabet only")
+for i in Alfabets:
+    
+    if ch == i:
+        print("FOUND!!",value)
+          
+    elif CH ==i:
+        print("FOUND!!",value+32)    
+    value=value+1      
+    
+   
