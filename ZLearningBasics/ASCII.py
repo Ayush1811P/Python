2f36a146b42a82d0877c9d8330c0bec3be058f70
Ayush Singh <awsmsi0405@gmail.com>
2026-01-21 23:59:15 +0530

    m

--- ZLearningBasics/ASCII.py
+++ ZLearningBasics/ASCII.py
@@ -17,6 +17,25 @@
 #         print("The ASCII value is: ",ascii)  
 #   
 
+
+# ch = input("Enter a character: ")
+# if len(ch)!=1:
+#     print("enter only one character!")
+# else:
+#     ascii=ord(ch)    #ord stands for ordinal value
+#     if ascii >=65 and ascii <=90:
+#         print("Uppercase Alfabet ",ch)
+#         print("The ASCII value is: ",ascii)
+#     elif ascii >97 and ascii < 122:
+#         print("lowercase Alfabet ",ch)  
+#         print("The ASCII value is: ",ascii) 
+#     else:
+#         print("Special character: ",ch)
+#         print("The ASCII value is: ",ascii)  
+#   
+
+
+
 # print(ord("H"))
 # print(ord("h"))
 
