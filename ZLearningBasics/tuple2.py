59edfd431f614a42b84f22a3d5075d1bffb66580
Ayush Singh <ayushhsingh1811@gmail.com>
2026-03-06 23:30:50 +0530

    updates

---
+++ ZLearningBasics/tuple2.py
@@ -0,0 +1,57 @@
+#person = ("Alice", 25, "Delhi")
+# name,age,city=person
+# print(name)
+# print(age)
+# print(city)
+
+
+
+# def min_max(nums):
+#     return min(nums),max(nums)
+
+# result = min_max([3, 1, 4, 1, 5, 9])
+# print(result)
+
+# low,high=min_max([3, 1, 4, 1, 5, 9])
+# print(f"MIN: {low} MAX: {high}")
+
+
+# students = (
+#     ("Alice", 85),
+#     ("Bob", 92),
+#     ("Diana", 95)
+# )
+# # print(type(students))
+# for name,score in students:
+#     print(f"{name} scored {score}")
+
+
+# def student_stats(students):
+#     total = 0
+#     top_student = ""
+#     max_score = 0
+
+#     for name, score in students:
+#         total += score
+        
+#         if score > max_score:
+#             max_score = score
+#             top_student = name
+
+#     average_score = total / len(students)
+
+#     return (top_student, average_score)
+
+
+# students = [
+#     ("Alice", 85),
+#     ("Bob", 92),
+#     ("Charlie", 78),
+#     ("Diana", 95),
+#     ("Eve", 88)
+# ]
+
+# print(student_stats(students))
+
+
+
